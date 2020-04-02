b642c42eb57e0714c23ebdca325b2e90a4827df9
rezasyariefsn <rezasyariefsn@gmail.com>
2020-04-02 10:47:12 +0700

    Update Home Fragment

--- app/src/main/java/com/example/aplikasita/HomeFragment.java
+++ app/src/main/java/com/example/aplikasita/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.aplikasita;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,12 +9,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    private Button gotosavefilter;
 
 
     public HomeFragment() {
@@ -25,7 +29,18 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        gotosavefilter = view.findViewById(R.id.goToSaveFilter);
+
+        gotosavefilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), filter.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 
 }
